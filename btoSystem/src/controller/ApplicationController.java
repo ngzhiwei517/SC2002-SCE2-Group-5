6b9653fc3fd26d59622641dd2c638dc2f4c8aafc
zhiweing <ngzhiwei03@gmail.com>
2025-03-21 02:04:08 +0800

    add method of application controller

--- btoSystem/src/controller/ApplicationController.java
+++ btoSystem/src/controller/ApplicationController.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ApplicationController {
-    private List<Application> applications;
+    private final List<Application> applications;
     private HDBManager manager;
 
     public ApplicationController(HDBManager manager) {
